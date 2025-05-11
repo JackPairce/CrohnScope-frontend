03d034e958d4cdf641eda59880b785b10847722f
JackPairce <worldofspeeders@gmail.com>
2025-05-11 08:39:17 +0100

    Rerouting Data

---
+++ src/app/data/sergmentation/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Segmentation Canvas",
+  description: "Segmentation Canvas for CrohnScope",
+};
+
+const SegmentationCanvas = () => {
+  return <div>Segmentation Canvas</div>;
+};
+
+export default SegmentationCanvas;
