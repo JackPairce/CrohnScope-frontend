b6defd67d22af3afb667f2f3268cd9e57043b6ee
JackPairce <st.4.melhout.202031004493@gmail.com>
2025-05-20 21:23:54 +0100

    Removes duplicate page components in ImagesNav
    Consolidates duplicate page components by removing redundant files that served the same purpose. Both files were exporting identical async page components that rendered the same ImageAside component.
    
    This cleanup helps maintain a cleaner codebase structure and reduces confusion by eliminating duplicated code.

--- src/components/AnnotationCanvas/ImagesNav/default.tsx
+++
@@ -1,5 +0,0 @@
-import ImagesAside from "./component";
-
-export default async function Page() {
-  return <ImagesAside />;
-}
--- src/components/AnnotationCanvas/ImagesNav/page.tsx
+++
@@ -1,4 +0,0 @@
-import ImageAside from "./component";
-export default async function Page() {
-  return <ImageAside />;
-}
